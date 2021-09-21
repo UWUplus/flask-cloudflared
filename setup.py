818f1d8c2989ba81e6d50977ab19ad25c0cf190e
Ralf <ralf@uwu.plus>
2021-09-21 14:43:17 +0200

    bumped version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="flask_cloudflared",
-    version="0.0.4",
+    version="0.0.5",
     author="Ralf Rademacher",
     description="Start a TryCloudflare Tunnel from your flask app.",
     long_description=long_description,
